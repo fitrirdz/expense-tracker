f004726e352d4ff904e5e664a77258dc69284278
Fitri Ratna Dewi <fratnadewi@gmail.com>
2025-08-10 00:33:40 +0700

    fix theming

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,14 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import './globals.css'
-
-export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
+import './globals.css';
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
         <style>{`
 html {
@@ -27,5 +20,5 @@ html {
       </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
